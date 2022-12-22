68d6f82538c158564f8a00e0eed0950575df1ee9
jeffassis <cmdtjeff@gmail.com>
2022-12-21 23:38:33 -0300

    criando querys genericas na classe banco

--- Project_Odin/Banco.cs
+++ Project_Odin/Banco.cs
@@ -20,5 +20,52 @@ namespace Project_Odin
             conexao.Open();
             return conexao;
         }
+
+        public static DataTable dql(string sql)
+        {
+            SQLiteDataAdapter da = null;
+            DataTable dt = new DataTable();
+            try
+            {
+                var vcon = ConexaoBanco();
+                var cmd = vcon.CreateCommand();
+                cmd.CommandText = sql;
+                da = new SQLiteDataAdapter(cmd.CommandText, vcon);
+                da.Fill(dt);
+                vcon.Close();
+                return dt;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static void dml(string q, string msgOK = null, string msgError = null)
+        {
+            SQLiteDataAdapter da = null;
+            DataTable dt = new DataTable();
+            try
+            {
+                var vcon = ConexaoBanco();
+                var cmd = vcon.CreateCommand();
+                cmd.CommandText = q;
+                da = new SQLiteDataAdapter(cmd.CommandText, vcon);
+                cmd.ExecuteNonQuery();
+                vcon.Close();
+                if (msgOK != null)
+                {
+                    MessageBox.Show(msgOK);
+                }
+            }
+            catch (Exception ex)
+            {
+                if (msgError != null)
+                {
+                    MessageBox.Show(msgError + "\n" + ex.Message);
+                }
+                throw ex;
+            }
+        }
     }
 }
