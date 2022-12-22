c2eb9d7d0d25c196c61ab7dfab646c5105194890
jeffassis <cmdtjeff@gmail.com>
2022-12-22 08:11:49 -0300

    add function form control

--- Project_Odin/Form1.cs
+++ Project_Odin/Form1.cs
@@ -7,6 +7,26 @@ namespace Project_Odin
             InitializeComponent();
         }
 
+        // Funcao para abir os formularios com parametros de nivel e chamada do Form
+        private void abreForm(int nivel, Form f)
+        {
+            if (Globais.logado)
+            {
+                if (Globais.nivel >= nivel)
+                {
+                    f.ShowDialog();
+                }
+                else
+                {
+                    MessageBox.Show("Acesso nao permitido");
+                }
+            }
+            else
+            {
+                MessageBox.Show("E necessario ter um usuario logado");
+            }
+        }
+
         private void Form1_Load(object sender, EventArgs e)
         {
 
